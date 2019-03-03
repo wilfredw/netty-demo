f83cdefb84b2c5ead8ae7a33c2ac735913ff3309
Wang Wei <wei@localhost.localdomain>
2019-03-03 22:14:03 +0800

    hi-server

---
+++ hi-server/src/main/java/com/wei/netty/hi/server/NettyNioServer.java
@@ -0,0 +1,47 @@
+package com.wei.netty.hi.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+
+
+public class NettyNioServer {
+
+    public void server(int port) throws Exception {
+        final ByteBuf buf = Unpooled.unreleasableBuffer(
+                Unpooled.copiedBuffer("Hi!\r\n", Charset.forName("UTF-8")));
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();    //1
+            b.group(bossGroup, workerGroup)   //2
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {    //3
+                        @Override
+                        public void initChannel(SocketChannel ch)
+                                throws Exception {
+                            ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {    //4
+                                @Override
+                                public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                    ctx.writeAndFlush(buf.duplicate())                //5
+                                            .addListener(ChannelFutureListener.CLOSE);
+                                }
+                            });
+                        }
+                    });
+            ChannelFuture f = b.bind().sync();                    //6
+            f.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully().sync();                    //7
+            workerGroup.shutdownGracefully().sync();
+        }
+    }
+}
